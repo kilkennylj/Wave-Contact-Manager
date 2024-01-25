6e7e93fcae17bac6dea63e5fe0c0f3a916c3b6e3
arkaik2k01 <nobmiguel2001@gmail.com>
2024-01-25 18:40:49 -0500

    Initial upload

---
+++ js/login.js
@@ -0,0 +1,49 @@
+const urlBase = 'http://cop4331-33.xyz/';
+const extension = 'php';
+
+let userId=0;
+let userName='';
+
+function doLogin() {
+    
+    document.getElementById("loginResult").innerHTML = "Logging in...";
+    window.location.href = "landing.html";
+
+    // let login = document.getElementById("username").value;
+    // let password = document.getElementById("password").value;
+
+    // let tmp = {login:login, password:password};
+
+    // let jsonPayload = JSON.stringify(tmp);
+    // let url = urlBase + "/login" + extension;
+    // console.log("sending login request from "+login+ " to "+url);
+
+    // let xhr = new XMLHttpRequest();
+    // xhr.open("POST", url, true);
+    // xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+
+    // try
+    // {
+    //     xhr.onreadystatechange = function()
+    //     {
+    //         if(this.readyState == 4 && this.status == 200)
+    //         {
+    //             let jsonObject = JSON.parse(xhr.responseText);
+    //             userId = jsonObject.id;
+
+    //             if(userId < 1)
+    //             {
+    //                 document.getElementById("loginResult").innerHTML = "Incorrect password or user";
+    //                 return;
+    //             }
+
+    //             window.location.href = "landing.html";
+    //         }
+    //     };
+    //     xhr.send(jsonPayload);
+    // }
+    // catch(err)
+    // {
+    //     document.getElementById("loginResult").innerHTML = err.message;
+    // }
+}
