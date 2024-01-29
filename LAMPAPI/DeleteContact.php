eb9245d33223f40cdae79aeb8cb37c6978618f41
romandicesarefernandes <154945549+romandicesarefernandes@users.noreply.github.com>
2024-01-28 23:23:53 -0500

    Update DeleteContact.php

--- LAMPAPI/DeleteContact.php
+++ LAMPAPI/DeleteContact.php
@@ -1,11 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "myDB";
+
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331"); 
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
