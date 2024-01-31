5929e455a22de301774c8c1377ccc6c048187ea6
romandicesarefernandes <154945549+romandicesarefernandes@users.noreply.github.com>
2024-01-30 23:14:19 -0500

    Update UpdateContacts.php

--- LAMPAPI/UpdateContacts.php
+++ LAMPAPI/UpdateContacts.php
@@ -2,38 +2,51 @@
 
 $inData = getRequestInfo();
 
-$id = $inData["id"];
-// Additional parameters for update
-$name = $inData["name"];
-$email = $inData["email"];
-// Add other fields as needed
+// Extracting data from the input
+$id = $inData["UserID"];
+$firstName = $inData["FirstName"];
+$lastName = $inData["LastName"];
+$phoneNumber = $inData["Phone"];
+$emailAddress = $inData["Email"];
 
 // Create connection
 $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+
 // Check connection
 if ($conn->connect_error) {
     returnWithError($conn->connect_error);
 } else {
     // Update query
-    $stmt = $conn->prepare("UPDATE Contacts SET Name=?, Email=? WHERE ID = ?");
-    $stmt->bind_param("ssi", $name, $email, $id);
+    $stmt = $conn->prepare("UPDATE Contacts SET FirstName=?, LastName=?, Phone=?, Email=? WHERE ID = ?");
+    
+    // Bind parameters
+    $stmt->bind_param("ssssi", $firstName, $lastName, $phoneNumber, $emailAddress, $id);
+    
+    // Execute query
     $stmt->execute();
+    
+    // Close statement and connection
     $stmt->close();
     $conn->close();
+    
+    // Return success
     returnWithError("");
 }
 
+// Function to get request information
 function getRequestInfo()
 {
     return json_decode(file_get_contents('php://input'), true);
 }
 
+// Function to send result information as JSON
 function sendResultInfoAsJson($obj)
 {
     header('Content-type: application/json');
     echo $obj;
 }
 
+// Function to return an error
 function returnWithError($err)
 {
     $retValue = '{"error":"' . $err . '"}';
