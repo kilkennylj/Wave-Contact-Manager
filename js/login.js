8d1e8e20ef8f352a139d8487d4ac21214052896e
KSeney <157320133+KSeney@users.noreply.github.com>
2024-02-12 21:38:47 -0500

    Add Work

--- js/login.js
+++ js/login.js
@@ -4,7 +4,7 @@ const extension = 'php';
 let UserID_Global = 0;
 
 function doLogin() {
-    
+
     UserID_Global = 0;
 
     document.getElementById("loginResult").innerHTML = "Logging in...";
@@ -12,27 +12,23 @@ function doLogin() {
     let login = document.getElementById("username").value;
     let password = document.getElementById("password").value;
 
-    let tmp = {login:login, password:password};
+    let tmp = { login: login, password: password };
 
     let jsonPayload = JSON.stringify(tmp);
     let url = urlBase + "/Login." + extension;
-    console.log("sending login request from "+login+ " to "+url);
+    console.log("sending login request from " + login + " to " + url);
 
     let xhr = new XMLHttpRequest();
     xhr.open("POST", url, true);
     xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
 
-    try
-    {
-        xhr.onreadystatechange = function()
-        {
-            if(this.readyState == 4 && this.status == 200)
-            {
+    try {
+        xhr.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
                 let jsonObject = JSON.parse(xhr.responseText);
                 UserID_Global = jsonObject.id;
 
-                if(UserID_Global < 1)
-                {
+                if (UserID_Global < 1) {
                     document.getElementById("loginResult").innerHTML = "Incorrect password or user";
                     return;
                 }
@@ -42,8 +38,7 @@ function doLogin() {
         };
         xhr.send(jsonPayload);
     }
-    catch(err)
-    {
+    catch (err) {
         document.getElementById("loginResult").innerHTML = err.message;
     }
 }
@@ -54,8 +49,7 @@ function sendToRegister() {
 
 // Separate into a new file when done. Need to work out ID first
 
-function createRow()
-{
+function createRow() {
     let newRow = document.createElement('tr');
     newRow.innerHTML = `
             <td class="fName" id="fName" contenteditable="true">First Name</td>
@@ -67,80 +61,71 @@ function createRow()
                 <button class="cancelBtn" id="cancelBtn" onclick="cancelEdit(this, 'CancelNew')">Cancel</button>
             </td> 
         `;
-        document.getElementById("tableBody").appendChild(newRow);
+    document.getElementById("tableBody").appendChild(newRow);
 }
 
-function getContactID(LastName)
-{
+function getContactID(LastName) {
     let UserId = UserID_Global;
     let Search = LastName;
     let url = urlBase + "/SearchContacts." + extension;
 
-    let tmp = {UserId : UserId, Search : Search};
+    let tmp = { UserId: UserId, Search: Search };
     let jsonPayload = JSON.stringify(tmp);
 
     let xhr = new XMLHttpRequest("POST", url, true);
     xhr.open("POST", url, true);
     xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
     let contactID;
-    try
-    {
-        xhr.onreadystatechange = function()
-        {
-            if(this.readyState == 4 && this.status == 200)
-            {
+    try {
+        xhr.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
                 let jsonObject = JSON.parse(xhr.responseText);
                 contactID = jsonObject[0].ID;
             }
         };
         xhr.send(jsonPayload);
     }
-    catch(err)
-    {
+    catch (err) {
         console.log(err);
     }
 
     return contactID;
 }
 
-function saveUser(button, request)
-{
+function saveUser(button, request) {
     let thisRow = button.closest("tr");
     let tableCells = thisRow.getElementsByTagName("td");
-    let FirstName = tableCells[0].innerText;  //Get value of columns
-    let LastName = tableCells[1].innerText;
-    let Phone = tableCells[2].innerText;
-    let Email = tableCells[3].innerText;
+    let firstName = tableCells[0].innerText;  //Get value of columns
+    let lastName = tableCells[1].innerText;
+    let phone = tableCells[2].innerText;
+    let email = tableCells[3].innerText;
 
     let url;
     let tmp;
     //Saving edits to existing user
-    if(request === "SaveEdit")
-    {
+    if (request === "SaveEdit") {
         url = urlBase + "/UpdateContacts." + extension;
         let ID = parseInt(getContactID(LastName));
         tmp = {
-            FirstName:FirstName,
-            LastName:LastName,
-            Phone:Phone,
-            Email:Email,
-            ID:ID 
+            FirstName: firstName,
+            LastName: lastName,
+            Phone: phone,
+            Email: email,
+            ID: ID
         }
     }
-    else if(request === "SaveNew")
-    {
+    else if (request === "SaveNew") {
         url = urlBase + "/AddContacts." + extension;
-        let UserID = UserID_Global;
+        let userID = UserID_Global;
         tmp = {
-            FirstName:FirstName,
-            LastName:LastName,
-            Phone:Phone,
-            Email:Email,
-            UserID:UserID 
+            firstName: firstName,
+            lastName: lastName,
+            phone: phone,
+            email: email,
+            userID: userID
         }
     }
-    else
-    {
+    else {
         console.log("HTML ERROR: Invalid or No request given to save function");
         return;
     }
@@ -149,17 +134,14 @@ function saveUser(button, request)
     let xhr = new XMLHttpRequest("POST", url, true);
     xhr.open("POST", url, true);
     xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-    try
-    {
-        xhr.onreadystatechange = function()
-        {
-            if(this.readyState == 4 && this.status == 200)
-            {
+    try {
+        xhr.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
                 thisRow.innerHTML = `
-                <td class="fName" id="fName">`+FirstName+`</td>
-                <td class="lName" id="lName">`+LastName+`</td>
-                <td class="phone" id="phone">`+Phone+`</td>
-                <td class="email" id="email">`+Email+`</td>
+                <td class="fName" id="fName">`+ firstName + `</td>
+                <td class="lName" id="lName">`+ lastName + `</td>
+                <td class="phone" id="phone">`+ phone + `</td>
+                <td class="email" id="email">`+ email + `</td>
                 <td class="button" id="button" type="button">
                     <button class="editBtn" id="editBtn" onclick="editUser(this)">Edit</button>
                     <button class="delBtn" id="delBtn" onclick="deleteUser(this)">Delete</button>
@@ -169,15 +151,13 @@ function saveUser(button, request)
         };
         xhr.send(jsonPayload);
     }
-    catch(err)
-    {
+    catch (err) {
         console.log(err);
     }
 }
 
 //Changes text fields to be editable and sets Save button
-function editUser(button)
-{
+function editUser(button) {
     let thisRow = button.closest("tr");
     let tableCells = thisRow.getElementsByTagName("td");
     let FirstName = tableCells[0].innerText;  //Save value of columns
@@ -186,10 +166,10 @@ function editUser(button)
     let Email = tableCells[3].innerText;
 
     thisRow.innerHTML = `
-                    <td class="fName" id="fName" contenteditable="true">`+FirstName+`</td>
-                    <td class="lName" id="lName" contenteditable="true">`+LastName+`</td>
-                    <td class="phone" id="phone" contenteditable="true">`+Phone+`</td>
-                    <td class="email" id="email" contenteditable="true">`+Email+`</td>
+                    <td class="fName" id="fName" contenteditable="true">`+ FirstName + `</td>
+                    <td class="lName" id="lName" contenteditable="true">`+ LastName + `</td>
+                    <td class="phone" id="phone" contenteditable="true">`+ Phone + `</td>
+                    <td class="email" id="email" contenteditable="true">`+ Email + `</td>
                     <td class="button" id="button" type="button">
                         <button class="editBtn" id="editBtn" onclick="saveUser(this, 'SaveEdit')">Save</button>
                         <button class="cancelBtn" id="cancelBtn" onclick="cancelEdit(this, 'CancelExists')">Cancel</button>
@@ -197,22 +177,20 @@ function editUser(button)
                     `;
 }
 
-function deleteUser (button)
-{
+function deleteUser(button) {
     let confirmed = window.confirm("Do you want to delete this contact?");
-    if(confirmed)
-    {
+    if (confirmed) {
         let thisRow = button.closest("tr");
         let tableCells = thisRow.getElementsByTagName("td");
         let firstName = tableCells[0].innerText;
         let lastName = tableCells[1].innerText;
         let UserID = UserID_Global;
 
-        let tmp = 
+        let tmp =
         {
-            firstName : firstName,
-            lastName : lastName,
-            UserID : UserID
+            firstName: firstName,
+            lastName: lastName,
+            UserID: UserID
         }
 
         let jsonPayload = JSON.stringify(tmp);
@@ -220,96 +198,80 @@ function deleteUser (button)
         let xhr = new XMLHttpRequest("POST", url, true);
         xhr.open("POST", url, true);
         xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-        try
-        {
-            xhr.onreadystatechange = function()
-            {
-                if (this.readyState == 4 && this.status == 200)
-                {
+        try {
+            xhr.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 200) {
                     deleteRow(button);
                 }
             };
             xhr.send(jsonPayload);
         }
-        catch(err)
-        {
+        catch (err) {
             console.log(err);
         }
     }
-    else
-    {
+    else {
         console.log("Deletion canceled.");
     }
 }
 
-function deleteRow(button)
-{
+function deleteRow(button) {
     button.closest("tr").remove();
 }
 
-function cancelEdit(button, request)
-{
+function cancelEdit(button, request) {
     let thisRow = button.closest("tr");
-    if(request === "CancelNew")
-    {
+    if (request === "CancelNew") {
         thisRow.remove();
     }
-    else if(request === "CancelExists")
-    {
+    else if (request === "CancelExists") {
         let tableCells = thisRow.getElementsByTagName("td");
         let FirstName = tableCells[0].innerText;  //Save value of columns
         let LastName = tableCells[1].innerText;
         let Phone = tableCells[2].innerText;
         let Email = tableCells[3].innerText;
         thisRow.innerHTML = `
-                <td class="fName" id="fName">`+FirstName+`</td>
-                <td class="lName" id="lName">`+LastName+`</td>
-                <td class="phone" id="phone">`+Phone+`</td>
-                <td class="email" id="email">`+Email+`</td>
+                <td class="fName" id="fName">`+ FirstName + `</td>
+                <td class="lName" id="lName">`+ LastName + `</td>
+                <td class="phone" id="phone">`+ Phone + `</td>
+                <td class="email" id="email">`+ Email + `</td>
                 <td class="button" id="button" type="button">
                     <button class="editBtn" id="editBtn" onclick="editUser(this)">Edit</button>
                     <button class="delBtn" id="delBtn" onclick="deleteUser(this)">Delete</button>
                 </td> 
                 `;
     }
-    else
-    {
+    else {
         console.log("HTML ERROR: Invalid or no request given to function");
     }
 }
 
-function searchContacts()
-{
+function searchContacts() {
     let Search = document.getElementById("searchQuery").value;
     let UserID = UserID_Global;
 
-    let tmp = {UserId : UserId, Search : Search};
+    let tmp = { UserId: UserId, Search: Search };
     let jsonPayload = JSON.stringify(tmp);
 
     let url = urlBase + '/SearchContacts.' + extension;
     let xhr = new XMLHttpRequest;
     xhr.open("POST", url, true);
     xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-    try
-    {
-        xhr.onreadystatechange = function()
-        {
-            if(this.readyState == 4 && this.status == 200)
-            {
+    try {
+        xhr.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
                 let jsonObject = JSON.parse(xhr.responseText);
                 updateTable(jsonObject);
             }
         };
         xhr.send(jsonPayload);
     }
-    catch(err)
-    {
+    catch (err) {
         console.log(err);
     }
 }
 
-function updateTable(data)
-{
+function updateTable(data) {
     let table = document.getElementById("contactTable");
     let tbody = table.getElementsByTagName("tbody");
     tbody.innerHTML = ""; //Clear table
@@ -317,8 +279,7 @@ function updateTable(data)
     data.array.forEach(item => {
         let row = document.createElement("tr");
         let keys = Object.keys(item);
-        for(var i = 0; i < keys.length - 1; i++)
-        {
+        for (var i = 0; i < keys.length - 1; i++) {
             let cell = document.createElement("td");
             cell.textContent = item[keys[i]];
             row.appendChild(cell);
@@ -331,8 +292,7 @@ function updateTable(data)
     });
 }
 
-function logout()
-{
+function logout() {
     UserID_Global = 0;
     window.location.href = "index.html"
 }
