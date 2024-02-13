60efdc0fe7740167483e7441f6eb594f051e0729
KSeney <157320133+KSeney@users.noreply.github.com>
2024-02-12 20:22:54 -0500

    Can search for email and phone as well

--- LAMPAPI/SearchContacts.php
+++ LAMPAPI/SearchContacts.php
@@ -12,9 +12,9 @@
 	} 
 	else
 	{
-		$stmt = $conn->prepare("select * from Contacts where (FirstName like ? or LastName like ?) and UserID=?");
+		$stmt = $conn->prepare("select * from Contacts where (FirstName like ? or LastName like ? or Phone like ? or Email like ?) and UserID=?");
 		$Name = "%" . $inData["search"] . "%";
-		$stmt->bind_param("sss", $Name, $Name, $inData["UserId"]);
+		$stmt->bind_param("sssss", $Name, $Name, $Name, $Name, $inData["UserId"]);
 		$stmt->execute();
 		
 		$result = $stmt->get_result();
@@ -26,7 +26,7 @@
 				$searchResults .= ",";
 			}
 			$searchCount++;
-			$searchResults .= '{"FirstName" : "' . $row["FirstName"]. '", "LastName" : "' . $row["LastName"]. '", "Email" : "' . $row["Email"]. '", "Phone" : "' . $row["Phone"]. '", "UserID" : "' . $row["UserID"].'", "ID" : "' . $row["ID"].'"}';
+			$searchResults .= '{"FirstName" : "' . $row["FirstName"]. '", "LastName" : "' . $row["LastName"]. '", "Email" : "' . $row["Email"]. '", "Phone" : "' . (int)$row["Phone"]. '", "UserID" : "' . (int)$row["UserID"].'", "ID" : "' . (int)$row["ID"].'"}';
 		}
 		
 		if( $searchCount == 0 )
